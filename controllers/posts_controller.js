1687512e78dd08078cd17935d4048d38b421ae07
Govardhana Lakshminarasimhaiah(UST <178376@ust-global.com>
2023-08-10 23:37:23 +0530

    Deleted posts and comments of that post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Posts = require('../models/posts')
+const Comment = require('../models/comments');
 
 module.exports.createPosts = function(req,res){
     console.log({content:req.body.content,
@@ -18,3 +19,30 @@ module.exports.createPosts = function(req,res){
 }
 
 
+module.exports.destroyPost = function(req,res){
+    Posts.findById(req.params.id)
+    .then(function(post){
+        //.id meand we are converting object id to string, whnever we need to compare object IDs we need to convert to string
+        console.log(post)
+        if (post.user == req.user.id){
+            //post.remove()// depricated: this removes post from DB, mongoose itself searches id of post and deletes it
+            Posts.findByIdAndDelete(post.id)
+            .then(function(postdeleted){
+                Comment.deleteMany({post:req.params.id})
+                .then(function(commentdeleted){
+                    console.log('Deleted',postdeleted, commentdeleted);
+                    return res.redirect('back');
+                })
+                .catch(function(err){
+                    console.log(`Error occured while deleting comments of a ${post.user} ${err}`);
+                })
+            })
+            .catch(function(err){
+                console.log(`Error occured while deleting post of a ${post.user} ${err}`);
+            })
+        }
+    })
+    .catch(function(err){
+        console.log(`Error occured while fetching post to delete ${err}`);
+    })
+}
--- routes/posts.js
+++ routes/posts.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const posts_controller = require('../controllers/posts_controller');
 
 router.post('/create-comments', posts_controller.createPosts);
+router.get('/delete/:id',posts_controller.destroyPost);
 
 module.exports = router;
