123234a3758958250e29c75ec33fef5b6428211e
Govardhana Lakshminarasimhaiah(UST <178376@ust-global.com>
2023-08-11 00:26:33 +0530

    Deleted comment and comment in post schema

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -25,3 +25,30 @@ module.exports.createComment = function(req,res){
         console.log(`Error occured while searching posts to comment ${err}`);
     })
 }
+
+
+module.exports.deleteComment = function(req,res){
+    Comments.findById(req.params.id)
+    .then(function(cmnt){
+        if (cmnt.user == req.user.id){
+            let postID = cmnt.post
+            Comments.findByIdAndDelete(cmnt.id)
+            .then(function(deletedcomment){
+                Posts.findByIdAndUpdate(postID, {$pull: {comment:req.params.id}})// this is a default function where it pulls out the data from array and update
+                .then(function(deletedFromPostArray){
+                    console.log(`deleted comment fromm both posts and comment schema ${deletedcomment}`)
+                    res.redirect('back');
+                })
+                .catch(function(err){
+                    console.log(`Error occured while  pulling comment from posts array ${err}`);
+                })
+            })
+            .catch(function(err){
+                console.log(`Error occured while  deleting comment ${err}`);
+            })
+        }
+    })
+    .catch(function(err){
+        console.log(`Error occured while searching comment in DB ${err}`);
+    })
+}
--- routes/comments.js
+++ routes/comments.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const comments_controller = require('../controllers/comments_controller');
 
 router.post('/create-comments',comments_controller.createComment);
+router.get('/delete/:id', comments_controller.deleteComment);
 
 module.exports = router;
